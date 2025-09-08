b58a6617122c1441cc71ec2a1bfb90b6275db338
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-08 10:42:00 +0700

    scaffolding project for vite

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  envPrefix: ['APP_'],
+
+  server: {
+    port: 3000,
+  },
+
+  plugins: [tsConfigPaths(), react(), tailwindcss()],
+});
