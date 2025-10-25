40c49919417662bf5763586ddd98d27f1bb112f7
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-10-25 11:31:04 +0700

    refactor: remove unused tw-animate-css dependency and clean up main.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import '~/styles/main.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-import '~/styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
